5eddce74d5a6aa2ba8f5bdf224907380bdec7761
Gracy <gracy331.be22@chitkara.edu.in>
2024-05-04 13:39:42 +0530

    first commit

---
+++ src/components/Timer.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+const Timer = ({ setTimeOut, questionNumber }) => {
+	const [timer, setTimer] = useState(30);
+
+	useEffect(() => {
+		if (timer === 0) return setTimeOut(true);
+		const interval = setInterval(() => {
+			setTimer(prev => prev - 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, [timer, setTimeOut]);
+
+	useEffect(() => {
+		if (timer === 0) {
+			setTimeOut(true);
+			return;
+		}
+		setTimer(30);
+	}, [questionNumber]);
+	return timer;
+};
+
+export default Timer;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// ReactDOM is a package that provides DOM-specific methods that can be used at the top level of your app
+// and as a part of the server rendering process. Most of your components should not need to use this module.
+// Instead, consider using functional components.
+// DOM : Document Object Model which basically represents the structure of the objects in the web page.
+// ReactDOM is basically a DOM-specific package that is used to render the components in the DOM.
+import ReactDOM from 'react-dom';
 import './index.css';
+// App is functionial component that is imported from App.js file.
 import App from './App';
+// mdb-react-ui-kit is a package that provides a lot of components that can be used to build the UI of the app.
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// reportWebVitals is a function that is imported from reportWebVitals.js file. It is used to measure the performance of the app.
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ReactDOM.render() is a function that is used to render the components in the DOM.
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
